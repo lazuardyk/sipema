efbc95aa208cd75c4500d8d3d25fe8e974f345e2
Lazuardy Khatulistiwa <dimazkid@gmail.com>
2019-08-21 23:24:44 +0700

    create migrate

---
+++ database/migrations/2019_08_21_161623_create_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_makanan');
+            $table->string('nama_kantin');
+            $table->integer('harga');
+            $table->string('path_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
