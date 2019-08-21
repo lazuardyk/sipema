6d90e28da4354616b8d1ff9e492362abd2aa7b15
Lazuardy Khatulistiwa <dimazkid@gmail.com>
2019-08-21 23:09:28 +0700

    create form in add food page

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -23,15 +23,41 @@
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
         <a class="nav-item nav-link" href="/"><i class="fas fa-home"></i> Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link active" href="/about"><i class="fas fa-info"></i> Tentang</a>
-        <a class="nav-item nav-link" href="/tambah"><i class="fas fa-plus-circle"></i> Tambah Data</a>
+        <a class="nav-item nav-link" href="/about"><i class="fas fa-info"></i> Tentang</a>
+        <a class="nav-item nav-link active" href="/tambah"><i class="fas fa-plus-circle"></i> Tambah Data</a>
         </div>
     </div>
     </nav>
     <div class ="row">
-        <div class="col-md-6 text-center">
-            <p style="font-size: 40px;">Tambah Data</p>
+        <div class="col-md-6 text-center mx-5">
+            <p style="font-size: 40px;">Tambah Makanan</p>
+            <hr>
+            <p style="font-size: 20px;">Buat permintaan untuk menambahkan makanan! silahkan isi data berikut:</p>
+        <form>
+            <div class="form-group">
+                <label for="nama_makanan">Nama makanan</label>
+                <input type="text" class="form-control" id="nama_makanan" name="nama_makanan" placeholder="sate taichan">
+            </div>
+
+            <div class="form-group">
+                <label for="nama_kantin">Nama kantin</label>
+                <input type="text" class="form-control" id="nama_kantin" name="nama_kantin" placeholder="kantin blok M">
+            </div>
+
+            <div class="form-group">
+                <label for="harga">Harga</label>
+                <input type="number" class="form-control" id="harga" name="harga" placeholder="10000">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlFile1">Bukti menu</label>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit data</button>
+        </form>
         </div>
+
     </div>    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
